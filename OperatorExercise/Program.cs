35872db8a7e09175b01239ed14c5536c1197228f
Philip Kim <filshouse56@gmail.com>
2023-12-12 00:03:02 -0500

    Exercise Complted

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+
+            int sum = a + b;
+            int subtract = a - b;
+            int multiply = a * b;
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a}/{b} is {quotient} and the remainder is {remainder}");
+
+            Console.WriteLine("Enter the radius of your circle.");
+            var userInput = Console.ReadLine();
+
+            var radius = double.Parse(userInput);
+
+            Console.WriteLine(AreaOfCircle(radius));
+
+            var i = 3;
+            var j = 4;
+            var k = ++i * j++;
+
+            Console.WriteLine(k);  //output is 16
+        }
+        public static double AreaOfCircle(double radius)
+        {
+            var area= Math.PI * Math.Pow(radius, 2);
+            return area;
+            
+
+
         }
     }
 }
